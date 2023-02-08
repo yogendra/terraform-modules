c63dc38f495cf055b8f42185435cf5e35cc1175b
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2023-02-09 03:23:45 +0800

    Add vpc endpoint for s3, ssm, etc.

---
+++ yugabyte/infra/yb-vpc/endpoints.tf
@@ -0,0 +1,57 @@
+
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id            = aws_vpc.vpc.id
+  service_name      = "com.amazonaws.${var.region}.s3"
+  vpc_endpoint_type = "Gateway"
+  route_table_ids = aws_route_table.mgmt-route-table.*.id
+}
+
+
+
+resource "aws_vpc_endpoint" "ssm" {
+  vpc_id              = aws_vpc.vpc.id
+  service_name        = "com.amazonaws.${var.region}.ssm"
+  vpc_endpoint_type   = "Interface"
+  security_group_ids  = [aws_security_group.yba-node.id]
+  subnet_ids          = aws_subnet.mgmt-subnet.*.id
+  private_dns_enabled = true
+  tags = {
+    Name = format("ssm endpoint for vpc-%[1]s", aws_vpc.vpc.tags.Name)
+  }
+}
+
+resource "aws_vpc_endpoint" "ec2messages" {
+  vpc_id              = aws_vpc.vpc.id
+  service_name        = "com.amazonaws.${var.region}.ec2messages"
+  vpc_endpoint_type   = "Interface"
+  security_group_ids  = [aws_security_group.yba-node.id]
+  subnet_ids          = aws_subnet.mgmt-subnet.*.id
+  private_dns_enabled = true
+  tags = {
+
+    Name = format("ec2messages endpoint for vpc-%[1]s", aws_vpc.vpc.tags.Name)
+  }
+}
+resource "aws_vpc_endpoint" "ssmmessages" {
+  vpc_id              = aws_vpc.vpc.id
+  service_name        = "com.amazonaws.${var.region}.ssmmessages"
+  vpc_endpoint_type   = "Interface"
+  security_group_ids  = [aws_security_group.yba-node.id]
+  subnet_ids          = aws_subnet.mgmt-subnet.*.id
+  private_dns_enabled = true
+  tags = {
+    Name         = format("ssmmessages endpoint for vpc-%[1]s", aws_vpc.vpc.tags.Name)
+  }
+}
+
+
+resource "aws_vpc_endpoint" "ec2" {
+  vpc_id            = aws_vpc.vpc.id
+  service_name      = "com.amazonaws.${var.region}.ec2"
+  vpc_endpoint_type = "Interface"
+
+  security_group_ids  = [aws_security_group.yba-node.id]
+  subnet_ids          = aws_subnet.mgmt-subnet.*.id
+
+  private_dns_enabled = true
+}
