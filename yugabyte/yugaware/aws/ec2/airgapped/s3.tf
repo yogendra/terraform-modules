28d3130941948680200a087f51f83ee63be3dea2
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2023-02-21 00:28:10 +0800

    udpate for online and airgap support

--- yugabyte/yugaware/aws/ec2/airgapped/s3.tf
+++ yugabyte/yugaware/aws/ec2/airgapped/s3.tf
@@ -1,14 +1,14 @@
 // packages bucket
 // backup bucket
 
-resource "aws_s3_bucket" "yba-backup"{
-   bucket = "${var.prefix}-yba-backup"
+resource "aws_s3_bucket" "yba-backup" {
+  bucket = "${var.prefix}-yba-backup"
 }
 resource "aws_s3_bucket_acl" "yba-backup" {
   bucket = aws_s3_bucket.yba-backup.id
   acl    = "private"
 }
-resource "aws_s3_bucket" "yba-packages"{
+resource "aws_s3_bucket" "yba-packages" {
   bucket = "${var.prefix}-yba-packages"
 }
 resource "aws_s3_bucket_acl" "yba-package" {
@@ -16,18 +16,19 @@ resource "aws_s3_bucket_acl" "yba-package" {
   acl    = "private"
 }
 
-locals{
+locals {
   yb-version = replace(var.yb-release, "/-.+/", "")
 
   yba-airgap-filename = "yugaware-${var.yb-release}-linux-x86_64.airgap"
-  yba-airgap-url = "https://downloads.yugabyte.com/releases/${local.yb-version}/${local.yba-airgap-filename}"
+  yba-airgap-url      = "https://downloads.yugabyte.com/releases/${local.yb-version}/${local.yba-airgap-filename}"
 
-  replicated-url = "https://get.replicated.com/airgap"
+  replicated-url      = "https://get.replicated.com/airgap"
   replicated-filename = "replicated.tar.gz"
 
 }
 
 resource "null_resource" "yba-package" {
+  count = var.yba-online-install ? 0 : 1
   triggers = {
     package-hash = fileexists("${local.yba-airgap-filename}") ? filesha256("${local.yba-airgap-filename}") : ""
   }
@@ -38,8 +39,9 @@ resource "null_resource" "yba-package" {
 }
 
 resource "null_resource" "replicated-package" {
+  count = var.yba-online-install ? 0 : 1
   triggers = {
-    package-hash = fileexists("${local.replicated-filename}")? filesha256("${local.replicated-filename}"): ""
+    package-hash = fileexists("${local.replicated-filename}") ? filesha256("${local.replicated-filename}") : ""
   }
 
   provisioner "local-exec" {
@@ -48,18 +50,20 @@ resource "null_resource" "replicated-package" {
 }
 
 resource "aws_s3_object" "replicated-package" {
-  key        = "packages/replicated/${local.replicated-filename}"
-  bucket     = aws_s3_bucket.yba-packages.id
-  source     = "${local.replicated-filename}"
+  count  = var.yba-online-install ? 0 : 1
+  key    = "packages/replicated/${local.replicated-filename}"
+  bucket = aws_s3_bucket.yba-packages.id
+  source = local.replicated-filename
   depends_on = [
     null_resource.replicated-package
   ]
 }
 
 resource "aws_s3_object" "yba-package" {
-  key        = "packages/yugabyte/${local.yba-airgap-filename}"
-  bucket     = aws_s3_bucket.yba-packages.id
-  source     = "${local.yba-airgap-filename}"
+  count  = var.yba-online-install ? 0 : 1
+  key    = "packages/yugabyte/${local.yba-airgap-filename}"
+  bucket = aws_s3_bucket.yba-packages.id
+  source = local.yba-airgap-filename
   depends_on = [
     null_resource.yba-package
   ]
