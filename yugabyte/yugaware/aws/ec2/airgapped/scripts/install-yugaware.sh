fbfc406c4906207f7e5b94ea3763be444525600a
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2023-02-21 00:25:35 +0800

    Airgap and online support

--- yugabyte/yugaware/aws/ec2/airgapped/scripts/install-yugaware.sh
+++ yugabyte/yugaware/aws/ec2/airgapped/scripts/install-yugaware.sh
@@ -19,26 +19,35 @@ YW_API="$YB_PORTAL/api/v1"
 [[ ! -f ${REPLICATED_CONF} ]] && echo "${REPLICATED_CONF} missing" && exit 1
 [[ ! -f ${LICENCE_FILE} ]] && echo "${LICENCE_FILE} missing" && exit 1
 
-[[ ! -d ${PACKAGE_ROOT} ]] && echo "No package folder found. Downloading now" &&  aws s3 cp --no-progress --recursive ${STAGING_BUCKET}/packages ${PACKAGE_ROOT}
+ONLINE_INSTALL=${ONLINE_INSTALL:-1}
 
-[[ ! -f ${PACKAGE_ROOT}/replicated/replicated.tar.gz ]] && echo "${PACKAGE_ROOT}/replicated/replicated.tar.gz: missing" && exit 11
-
-ybpkg=$(cat ${REPLICATED_CONF} | grep LicenseBootstrapAirgapPackagePath | cut -d: -f2 | sed -e 's#.*"\(/.*\)",#\1#')
-[[ ! -f $ybpkg ]] &&  echo "[${ybpkg}]: missing" &&  exit 1
-
-
-echo "Unpack replicated"
-tar -C ${PACKAGE_ROOT}/replicated -xzvf ${PACKAGE_ROOT}/replicated/replicated.tar.gz
-
-echo "Update ${YUGAWARE_CONF}"
 HOST_IP=$(ip -4 -o addr show dev eth0 | awk {'print $4'} | cut -d/ -f1)
+echo "Update ${YUGAWARE_CONF}"
 sed -i "s/<hostname>/$HOST_IP/" ${YUGAWARE_CONF}
 
-(
-echo "Installing Replicated"
-cd ${PACKAGE_ROOT}/replicated
-./install.sh airgap local-address="$HOST_IP"  private-address="$HOST_IP" fast-timeouts
-)
+if [[ $ONLINE_INSTALL == "0"  ]]
+then
+
+  [[ ! -d ${PACKAGE_ROOT} ]] && echo "No package folder found. Downloading now" &&  aws s3 cp --no-progress --recursive ${STAGING_BUCKET}/packages ${PACKAGE_ROOT}
+
+  [[ ! -f ${PACKAGE_ROOT}/replicated/replicated.tar.gz ]] && echo "${PACKAGE_ROOT}/replicated/replicated.tar.gz: missing" && exit 11
+
+  ybpkg=$(cat ${REPLICATED_CONF} | grep LicenseBootstrapAirgapPackagePath | cut -d: -f2 | sed -e 's#.*"\(/.*\)",#\1#')
+  [[ ! -f $ybpkg ]] &&  echo "[${ybpkg}]: missing" &&  exit 1
+
+
+  echo "Unpack replicated"
+  tar -C ${PACKAGE_ROOT}/replicated -xzvf ${PACKAGE_ROOT}/replicated/replicated.tar.gz
+
+
+  (
+  echo "Installing Replicated"
+  cd ${PACKAGE_ROOT}/replicated
+  ./install.sh airgap local-address="$HOST_IP"  private-address="$HOST_IP" fast-timeouts
+  )
+else
+  curl -sSL https://get.replicated.com/docker | sudo bash -s -- local-address="$HOST_IP"  private-address="$HOST_IP" fast-timeouts no-proxy skip-preflights no-public-address
+fi
 echo "$YB_PORTAL: Not up. Waiting.."
 while ! curl -sSkLo /dev/null ${YW_API}/customer_count -f &> /dev/null
 do
