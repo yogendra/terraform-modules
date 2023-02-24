6b5c939035fd54c76d0d8eedf5509723a65d9582
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2023-02-24 17:27:45 +0800

    Fixed typo

--- yugabyte/infra/yb-vpc/keypair.tf
+++ yugabyte/infra/yb-vpc/keypair.tf
@@ -1,5 +1,5 @@
 resource "aws_key_pair" "keypair" {
   count = length(var.ssh_public_key)
-  prefkey_name_prefix = "${var.prefix}-${count.index}-key"
+  key_name_prefix = "${var.prefix}-${count.index}-key"
   public_key = file(var.ssh_public_keys[count.index])
 }
