d4756196713a0258844307b87c847b12c6bc07ea
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2023-02-27 01:48:59 +0800

    Update module ref path

--- yugabyte/infra/aws/vpc/test/airgapped/test.tf
+++ yugabyte/infra/aws/vpc/test/airgapped/test.tf
@@ -3,7 +3,7 @@ provider "aws" {
   region = "us-east-1"
 }
 module "test-account" {
-  source = "../../../yb-account"
+  source = "../../../account"
   prefix = local.project_config["prefix"]
 }
 module "test-vpc" {
