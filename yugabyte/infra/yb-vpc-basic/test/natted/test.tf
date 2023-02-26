b22c0652fd6d7d8a3189c6bfef262361375b7fcd
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2023-02-26 22:53:07 +0800

    Natted VPC test

--- yugabyte/infra/yb-vpc-basic/test/natted/test.tf
+++ yugabyte/infra/yb-vpc-basic/test/natted/test.tf
@@ -2,14 +2,46 @@
 provider "aws" {
   region = "us-east-1"
 }
-module "test" {
+module "test-account" {
+  source = "../../../yb-account"
+  prefix = local.project_config["prefix"]
+}
+module "test-vpc" {
   source = "../.."
-  region = "us-east-1"
-  prefix = "icn-test"
-  project_cidr = "10.212.0.0/16"
-  ssh_public_keys =  ["${path.cwd}/private/test.pub"]
-  airgapped = true
-  use_nat = false
   config = local.vpc_config["us-east-1"]
+  project_config = local.project_config
 }
 
+
+// Create jumbbox
+data "aws_ami" "test" {
+  most_recent = true
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+}
+
+resource "aws_instance" "test" {
+  ami           = data.aws_ami.test.id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [
+    module.test-vpc.sg-allow-internal,
+    module.test-vpc.sg-allow-egress
+  ]
+  subnet_id            = module.test-vpc.private-subnets[0]
+  iam_instance_profile = module.test-account.ssm-instance-profile
+  key_name             = module.test-vpc.ssh-keypairs[0]
+
+  tags = {
+    Name = "${local.project_config["prefix"]}-vm"
+  }
+}
+
+output "test-vm"{
+  value = aws_instance.test.id
+}
