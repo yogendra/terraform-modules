8bcf56286386477ea062cf5482129dcca15a4f69
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2023-02-27 01:05:59 +0800

    remove prefix list management

--- yugabyte/infra/yb-prefix-list/main.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_ec2_managed_prefix_list_entry" "se-ip" {
-  for_each = var.se-ips
-  cidr           = "${each.valye}/32"
-  description    = "IP for ${each.key}"
-  prefix_list_id = prefix_list_id
-}
--- yugabyte/infra/yb-prefix-list/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "vpc_id"{
-  type = string
-}
-variable "prefix_list_id"{
-  type = string
-}
-variable "se-ips"{
-  type = map(string)
-}
