c6165acf0c4ceaf32e6aeef1b7e1b90620216303
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2023-02-27 11:02:45 +0800

    Update az list

--- scripts/tf-generator.py
+++ scripts/tf-generator.py
@@ -11,7 +11,7 @@ def main():
 
   if generate == "default-az-list" :
       from json import dumps,loads
-      print(dumps(loads(DEFAULT_AZ_LIST)))
+      print(dumps(loads(DEFAULT_AZ_LIST), indent=2))
   if generate == 'all' or generate == 'providers':
       print(generate_provider(az_list, arg.tags))
   if generate == 'all' or generate == 'vpc-config':
@@ -363,24 +363,32 @@ class TagAction(argparse.Action):
 
 # update with aws cli command
 DEFAULT_AZ_LIST = """{
-      "ap-south-1"    : ["ap-south-1a"    , "ap-south-1b"    , "ap-south-1c"      ],
-      "ap-southeast-1": ["ap-southeast-1a", "ap-southeast-1b", "ap-southeast-1c"  ],
-      "ap-southeast-2": ["ap-southeast-2a", "ap-southeast-2b", "ap-southeast-2c"  ],
-      "ap-southeast-3": ["ap-southeast-3a", "ap-southeast-3b", "ap-southeast-3c"  ],
-      "ap-northeast-3": ["ap-northeast-3a", "ap-northeast-3b", "ap-northeast-3c"  ],
-      "ap-northeast-2": ["ap-northeast-2a", "ap-northeast-2b", "ap-northeast-2c", "ap-northeast-2d" ],
-      "ap-northeast-1": ["ap-northeast-1a", "ap-northeast-1c", "ap-northeast-1d"  ],
-      "ca-central-1"  : ["ca-central-1a"  , "ca-central-1b"  , "ca-central-1d"    ],
-      "eu-central-1"  : ["eu-central-1a"  , "eu-central-1b"  , "eu-central-1c"    ],
-      "eu-north-1"    : ["eu-north-1a"    , "eu-north-1b"    , "eu-north-1c"      ],
-      "eu-west-1"     : ["eu-west-1a"     , "eu-west-1b"     , "eu-west-1c"       ],
-      "eu-west-2"     : ["eu-west-2a"     , "eu-west-2b"     , "eu-west-2c"       ],
-      "eu-west-3"     : ["eu-west-3a"     , "eu-west-3b"     , "eu-west-3c"       ],
-      "sa-east-1"     : ["sa-east-1a"     , "sa-east-1b"     , "sa-east-1c"       ],
-      "us-east-1"     : ["us-east-1a"     , "us-east-1b"     , "us-east-1c"     , "us-east-1d", "us-east-1e", "us-east-1f"],
-      "us-east-2"     : ["us-east-2a"     , "us-east-2b"     , "us-east-2c"       ],
-      "us-west-1"     : ["us-west-1a"     , "us-west-1c"       ],
-      "us-west-2"     : ["us-west-2a"     , "us-west-2b"     , "us-west-2c"     , "us-west-2d"]
+  "af-south-1": ["af-south-1a", "af-south-1b", "af-south-1c"],
+  "ap-east-1": ["ap-east-1a", "ap-east-1b", "ap-east-1c"],
+  "ap-northeast-1": ["ap-northeast-1a", "ap-northeast-1c", "ap-northeast-1d"],
+  "ap-northeast-3": ["ap-northeast-3a", "ap-northeast-3b", "ap-northeast-3c"],
+  "ap-south-1": ["ap-south-1a", "ap-south-1b", "ap-south-1c"],
+  "ap-south-2": ["ap-south-2a", "ap-south-2b", "ap-south-2c"],
+  "ap-southeast-1": ["ap-southeast-1a", "ap-southeast-1b", "ap-southeast-1c"],
+  "ap-southeast-2": ["ap-southeast-2a", "ap-southeast-2b", "ap-southeast-2c"],
+  "ap-southeast-3": ["ap-southeast-3a", "ap-southeast-3b", "ap-southeast-3c"],
+  "ap-southeast-4": ["ap-southeast-4a", "ap-southeast-4b", "ap-southeast-4c"],
+  "ca-central-1": ["ca-central-1a", "ca-central-1b", "ca-central-1d"],
+  "eu-central-1": ["eu-central-1a", "eu-central-1b", "eu-central-1c"],
+  "eu-central-2": ["eu-central-2a", "eu-central-2b", "eu-central-2c"],
+  "eu-north-1": ["eu-north-1a", "eu-north-1b", "eu-north-1c"],
+  "eu-south-1": ["eu-south-1a", "eu-south-1b", "eu-south-1c"],
+  "eu-south-2": ["eu-south-2a", "eu-south-2b", "eu-south-2c"],
+  "eu-west-1": ["eu-west-1a", "eu-west-1b", "eu-west-1c"],
+  "eu-west-2": ["eu-west-2a", "eu-west-2b", "eu-west-2c"],
+  "eu-west-3": ["eu-west-3a", "eu-west-3b", "eu-west-3c"],
+  "me-central-1": ["me-central-1a", "me-central-1b", "me-central-1c"],
+  "me-south-1": ["me-south-1a", "me-south-1b", "me-south-1c"],
+  "sa-east-1": ["sa-east-1a", "sa-east-1b", "sa-east-1c"],
+  "us-east-1": [ "us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d", "us-east-1e", "us-east-1f" ],
+  "us-east-2": ["us-east-2a", "us-east-2b", "us-east-2c"],
+  "us-west-1": ["us-west-1a", "us-west-1b"],
+  "us-west-2": ["us-west-2a", "us-west-2b", "us-west-2c", "us-west-2d"]
     }"""
 if __name__  == '__main__':
   main()
