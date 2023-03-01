6497b9cab5a1a4b10529813428e96e829aea3fdc
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2023-03-01 23:50:47 +0800

    fix tls to use localhost

--- yugabyte/infra/aws/yugaware/tls.tf
+++ yugabyte/infra/aws/yugaware/tls.tf
@@ -1,11 +1,13 @@
 
 resource "tls_private_key" "portal" {
+  count = var.yba-tls-key-pem != "" ? 0 : 1
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
 resource "tls_self_signed_cert" "portal-01" {
-  private_key_pem = tls_private_key.portal.private_key_pem
+  count = var.yba-tls-key-pem != "" ? 0 : 1
+  private_key_pem = tls_private_key.portal[0].private_key_pem
 
   subject {
     common_name  = "${var.prefix} - YugabyteDB Anywhere"
@@ -13,8 +15,7 @@ resource "tls_self_signed_cert" "portal-01" {
   }
 
   dns_names = [
-    "localhost",
-    "*.${var.internal-db-domain}"
+    "localhost"
   ]
   validity_period_hours = 1 * 365 * 24
 
