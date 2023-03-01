cd5d7150d84cfd0b8bd9fecd91a99e42027cb053
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2023-03-01 23:24:50 +0800

    regen

--- yugabyte/infra/aws/yugaware/test/test-vpc.tf
+++ yugabyte/infra/aws/yugaware/test/test-vpc.tf
@@ -5,7 +5,7 @@ module "vpc-ap-southeast-1" {
     aws = aws.ap_southeast_1
   }
   project_config = local.project_config
-  config         = local.vpc_config["ap-southeast-1"]
+  config = local.vpc_config.ap-southeast-1
 
 }
 
@@ -15,6 +15,32 @@ module "vpc-ap-southeast-2" {
     aws = aws.ap_southeast_2
   }
   project_config = local.project_config
-  config         = local.vpc_config["ap-southeast-2"]
+  config = local.vpc_config.ap-southeast-2
 
 }
+locals {
+    vpc_modules = [
+      "vpc-ap-southeast-1",
+      "vpc-ap-southeast-2"
+    ]
+  }
+locals {
+  vpc-by-region = {
+    for m in local.vpc_modules : m.config.region => {
+      allow-remote-prefix-list-id = m.allow-remote-prefix-list-id
+      sg-yba = m.sg-yba
+      sg-yb-db-nodes = m.sg-yb-db-nodes
+      sg-ingress = m.sg-ingress
+      sg-allow-remote = m.sg-allow-remote
+      sg-allow-internal = m.sg-allow-internal
+      sg-allow-egress = m.sg-allow-egress
+      ssh-keypairs = m.ssh-keypairs
+      private-subnet-by-az = m.private-subnet-by-az
+      private-subnets = m.private-subnets
+      public-subnet-by-az = m.public-subnet-by-az
+      public-subnets = m.public-subnets
+      vpc_id = m.vpc_id
+    }
+  }
+}
+
