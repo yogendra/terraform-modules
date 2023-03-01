208bb15d481ce60e6a89676876a5db085c1b011a
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2023-03-01 23:50:19 +0800

    added route53

--- yugabyte/infra/aws/yugaware/test/test.tf
+++ yugabyte/infra/aws/yugaware/test/test.tf
@@ -1,16 +1,17 @@
-
+resource "aws_route53_zone" "internal-db-zone" {
+  name = "${local.project_config.prefix}-ybdb.internal"
+  vpc {
+    vpc_id = module.vpc-ap-southeast-1.vpc_id
+  }
+}
 locals {
-  vpcs = [
-    module.vpc-ap-southeast-1,
-    module.vpc-ap-southeast-2
-  ]
   yb-provider = [
-    for vpc in local.vpcs: {
+    for vpc in local.vpc_modules : {
       name = vpc.region
-      sg = vpc.sg-yb-db-nodes
+      sg   = vpc.sg-yb-db-nodes
       zones = [
-        for zone, subnets in vpc.private-subnet-by-az: {
-          name = zone,
+        for zone, subnets in vpc.private-subnet-by-az : {
+          name   = zone,
           subnet = subnets[0]
         }
       ]
@@ -18,37 +19,39 @@ locals {
   ]
 
   aws-local = {
-    name = "aws-${local.project_config.prefix}"
-    air-gapped = false
-    internal-hosted-zone-id = ""
-    internal-hosted-domain = "",
-    ssh-port = 22
-    ssh-user = "ec2-user"
+    name                    = "aws-${local.project_config.prefix}"
+    air-gapped              = true
+    internal-hosted-zone-id = aws_route53_zone.internal-db-zone.zone_id
+    internal-hosted-domain  = "${local.project_config.prefix}-ybdb.internal",
+    ssh-port                = 22
+    ssh-user                = "ec2-user"
     regions = {
-      for vpc in local.vpcs: vpc.region => {
-        sg = vpc.sg-yb-db-nodes
-        architecture = "x86_64"
+      for vpc in local.vpc_modules : vpc.region => {
+        security-group = vpc.sg-yb-db-nodes
+        architecture   = "x86_64"
         az-subnets = {
-          for zone, subnets in vpc.private-subnet-by-az: zone => subnets[0]
+          for zone, subnets in vpc.private-subnet-by-az : zone => subnets[0]
         }
       }
     }
   }
 }
 
-module "yba"{
+
+module "yba" {
   source = "../"
   providers = {
     aws = aws.ap_southeast_1
   }
-  prefix = local.project_config.prefix
-  key-name = module.vpc-ap-southeast-1.key-names[0]
-  yba-subnet = module.vpc-ap-southeast-1.private-subnets[0]
-  yba-security-groups = [module.vpc-ap-southeast-1.sg-yba, module.vpc-ap-southeast-1.sg-allow-internal, module.vpc-ap-southeast-1.sg-allow-egress]
-  yba-license-file = "./private/yugaware.rli"
+  prefix                   = local.project_config.prefix
+  key-name                 = module.vpc-ap-southeast-1.key-names[0]
+  yba-subnet               = module.vpc-ap-southeast-1.private-subnets[0]
+  yba-security-groups      = [module.vpc-ap-southeast-1.sg-yba, module.vpc-ap-southeast-1.sg-allow-internal, module.vpc-ap-southeast-1.sg-allow-egress]
+  yba-license-file         = "./private/yugaware.rli"
   yba-iam-instance-profile = module.yb-account.yba-instance-profile
-  yba-online-install = true
-  
+  yba-online-install       = true
+  yba-aws-providers        = [local.aws-local]
+
 }
 
 
