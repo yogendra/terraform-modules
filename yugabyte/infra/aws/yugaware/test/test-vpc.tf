086bce70c4eb443f64206f6590412fb68fd16fd0
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2023-03-02 12:51:42 +0800

    regen

--- yugabyte/infra/aws/yugaware/test/test-vpc.tf
+++ yugabyte/infra/aws/yugaware/test/test-vpc.tf
@@ -41,6 +41,7 @@ locals {
       private-subnets = m.private-subnets
       public-subnet-by-az = m.public-subnet-by-az
       public-subnets = m.public-subnets
+      route-tables = m.route-tables
       vpc_id = m.vpc_id
     }
   }
