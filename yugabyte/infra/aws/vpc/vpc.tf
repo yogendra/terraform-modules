b655f6d4685ecbed3e7bedc8c791c9878dba0c02
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2023-03-02 13:42:36 +0800

    cleanup

--- yugabyte/infra/aws/vpc/vpc.tf
+++ yugabyte/infra/aws/vpc/vpc.tf
@@ -6,14 +6,10 @@ resource "aws_vpc" "vpc" {
   enable_dns_support = true
 
   tags = {
-    Name = "${local.prefix}-${local.region}"
+    Name = "${var.project_config.prefix}-${local.region}"
   }
 }
 resource "aws_vpc_ipv4_cidr_block_association" "secondary_cidr" {
-  # count = length(var.config.cidrs)-1
-  # vpc_id     = aws_vpc.vpc.id
-  # cidr_block =var.config.cidrs[count.index+1]
-
   for_each = toset(slice(var.config.cidrs, 1, length(var.config.cidrs)))
   vpc_id     = aws_vpc.vpc.id
   cidr_block = each.key
