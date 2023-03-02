75dddcef701159bc6bc7e5c2166acb56e77cd246
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2023-03-02 13:33:26 +0800

    cleanup local var

--- yugabyte/infra/aws/vpc/keypair.tf
+++ yugabyte/infra/aws/vpc/keypair.tf
@@ -1,5 +1,5 @@
 resource "aws_key_pair" "keypair" {
   count = length(var.project_config.ssh-public-keys)
-  key_name_prefix = "${local.prefix}-${count.index}-key"
+  key_name_prefix = "${var.project_config.prefix}-${count.index}-key"
   public_key = var.project_config.ssh-public-keys[count.index]
 }
