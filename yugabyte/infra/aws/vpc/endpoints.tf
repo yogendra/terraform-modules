7f8530548e0dc12e12b0f4b2b520c6e24d811459
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2023-04-14 02:08:20 +0800

    Add endpoints in all situation

--- yugabyte/infra/aws/vpc/endpoints.tf
+++ yugabyte/infra/aws/vpc/endpoints.tf
@@ -13,12 +13,12 @@ resource "aws_vpc_endpoint" "s3" {
 
 
 resource "aws_vpc_endpoint" "ssm" {
-  count               = local.create_endpoints?1:0
+  # count               = local.create_endpoints?1:0
   vpc_id              = aws_vpc.vpc.id
   service_name        = "com.amazonaws.${local.region}.ssm"
   vpc_endpoint_type   = "Interface"
   security_group_ids  = [aws_security_group.allow-internal.id]
-  subnet_ids          = aws_subnet.public[*].id
+  subnet_ids          = concat(aws_subnet.public[*].id, aws_subnet.private[*].id)
   private_dns_enabled = true
   tags = {
     Name = "${var.project_config.prefix}-ssm"
@@ -26,12 +26,12 @@ resource "aws_vpc_endpoint" "ssm" {
 }
 
 resource "aws_vpc_endpoint" "ec2messages" {
-  count               = local.create_endpoints?1:0
+  # count               = local.create_endpoints?1:0
   vpc_id              = aws_vpc.vpc.id
   service_name        = "com.amazonaws.${local.region}.ec2messages"
   vpc_endpoint_type   = "Interface"
   security_group_ids  = [aws_security_group.allow-internal.id]
-  subnet_ids          = aws_subnet.public[*].id
+  subnet_ids          = concat(aws_subnet.public[*].id, aws_subnet.private[*].id)
   private_dns_enabled = true
   tags = {
 
@@ -39,12 +39,12 @@ resource "aws_vpc_endpoint" "ec2messages" {
   }
 }
 resource "aws_vpc_endpoint" "ssmmessages" {
-  count               = local.create_endpoints?1:0
+  # count               = local.create_endpoints?1:0
   vpc_id              = aws_vpc.vpc.id
   service_name        = "com.amazonaws.${local.region}.ssmmessages"
   vpc_endpoint_type   = "Interface"
   security_group_ids  = [aws_security_group.allow-internal.id]
-  subnet_ids          = aws_subnet.public[*].id
+  subnet_ids          = concat(aws_subnet.public[*].id, aws_subnet.private[*].id)
   private_dns_enabled = true
   tags = {
     Name = "${var.project_config.prefix}-ssmmessages"
@@ -53,12 +53,12 @@ resource "aws_vpc_endpoint" "ssmmessages" {
 
 
 resource "aws_vpc_endpoint" "ec2" {
-  count             = local.create_endpoints?1:0
+  # count             = local.create_endpoints?1:0
   vpc_id            = aws_vpc.vpc.id
   service_name      = "com.amazonaws.${local.region}.ec2"
   vpc_endpoint_type = "Interface"
   security_group_ids  = [aws_security_group.allow-internal.id]
-  subnet_ids          = aws_subnet.public[*].id
+  subnet_ids          = concat(aws_subnet.public[*].id, aws_subnet.private[*].id)
   private_dns_enabled = true
   tags = {
     Name = "${var.project_config.prefix}-ec2"
@@ -67,12 +67,12 @@ resource "aws_vpc_endpoint" "ec2" {
 
 
 resource "aws_vpc_endpoint" "kms" {
-  count             = local.create_endpoints?1:0
+  # count             = local.create_endpoints?1:0
   vpc_id            = aws_vpc.vpc.id
   service_name      = "com.amazonaws.${local.region}.kms"
   vpc_endpoint_type = "Interface"
   security_group_ids  = [aws_security_group.allow-internal.id]
-  subnet_ids          = aws_subnet.public[*].id
+  subnet_ids          = concat(aws_subnet.public[*].id, aws_subnet.private[*].id)
   private_dns_enabled = true
   tags = {
     Name = "${var.project_config.prefix}-kms"
@@ -81,12 +81,12 @@ resource "aws_vpc_endpoint" "kms" {
 
 
 resource "aws_vpc_endpoint" "cloudwatch" {
-  count             = local.create_endpoints?1:0
+  # count             = local.create_endpoints?1:0
   vpc_id            = aws_vpc.vpc.id
   service_name      = "com.amazonaws.${local.region}.logs"
   vpc_endpoint_type = "Interface"
   security_group_ids  = [aws_security_group.allow-internal.id]
-  subnet_ids          = aws_subnet.public[*].id
+  subnet_ids          = concat(aws_subnet.public[*].id, aws_subnet.private[*].id)
   private_dns_enabled = true
   tags = {
     Name = "${var.project_config.prefix}-cloudwatch"
