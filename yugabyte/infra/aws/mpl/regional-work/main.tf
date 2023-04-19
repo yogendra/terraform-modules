a99c64be5f8d515758be0a322f19e50a526f923c
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2023-04-19 19:34:52 +0800

    Add mpl region work module

---
+++ yugabyte/infra/aws/mpl/regional-work/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+variable "prefix-list-name"{
+  type = string
+  description = "Name of the prefix list to update"
+}
+
+variable "cidrs" {
+  type = list(string)
+  description = "List of CIDRs to associate with entry"
+}
+variable "description" {
+  type = string
+  description = "decription for mpl entry"
+}
+
+
+data "aws_ec2_managed_prefix_list" "prefix_list"{
+  name = var.prefix_list_name
+}
+
+resource "aws_ec2_managed_prefix_list_entry" "entries" {
+  count          = length(var.cidrs)
+  cidr           = var.cidrs[count.index]
+  description    = "${var.description} [${count.index}]"
+  prefix_list_id = data.aws_ec2_managed_prefix_list.prefix_list.id
+}
