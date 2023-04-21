7bd5ceb2a99dd56d752c74183ec082ab23a91c76
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2023-04-22 02:38:46 +0800

    Added vars to make the run more deterministic

--- yugabyte/infra/aws/vpc-peering/variable.tf
+++ yugabyte/infra/aws/vpc-peering/variable.tf
@@ -3,11 +3,29 @@ variable "src_vpc_id" {
   type = string
   description = "Source VPC ID to peer"
 }
-
+variable "src_cidrs" {
+  type = list(string)
+  description = "Source VPC CIDRs. Optional if only source to destination routing."
+  default = []
+}
+variable "src_route_tables"{
+  type = list(string)
+  description = "Source VPC Route Tables"
+}
 variable "dest_vpc_id" {
   type = string
   description = "Destination VPC ID"
 }
-variable "prefix" {
+variable "dest_cidrs" {
+  type = list(string)
+  description = "Destination VPC CIDRs"
+}
+variable "dest_route_tables"{
+  type = list(string)
+  description = "Destination VPC Route Tables. Optional if only source to destination routing."
+  default = []
+}
+variable "dest_region" {
   type = string
+  description = "Destination Region Name"
 }
