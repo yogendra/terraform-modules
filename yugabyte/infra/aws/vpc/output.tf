13891822eb7a70f1fc58188b4a357a35535d03d7
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2023-05-05 22:17:23 +0800

    update to match the sg

--- yugabyte/infra/aws/vpc/output.tf
+++ yugabyte/infra/aws/vpc/output.tf
@@ -6,25 +6,21 @@ output "vpc_id"{
   value = aws_vpc.vpc.id
 }
 
-output "sg-ingress" {
-  value = aws_security_group.allow-ingress.id
-}
-output "sg-allow-internal" {
-  value = aws_security_group.allow-internal.id
+output "sg-app" {
+  value = aws_security_group.app.id
 }
 output "sg-yba" {
   value = aws_security_group.yba-node.id
 }
-output "sg-allow-remote"{
-  value = one(aws_security_group.allow-remote.*.id)
-}
-
-output "sg-allow-egress"{
-  value = aws_security_group.allow-egress.id
-}
 
 output "sg-yb-db-nodes"{
   value = aws_security_group.yb-db-nodes.id
+}
+output "sg-default" {
+  value = aws_vpc.vpc.default_security_group_id
+}
+output "mpl"{
+  value = one(aws_ec2_managed_prefix_list.allow-remote.*.id)
 }
 
 output "public-subnets" {
@@ -46,6 +42,15 @@ output "public-subnet-by-az"{
   value = {
     for subnet in aws_subnet.public[*] : subnet.availability_zone => subnet.id...
   }
+}
+output "route-tables-private"{
+  value = aws_route_table.private.*.id
+}
+output "route-tables-public"{
+  value = aws_route_table.public.*.id
+}
+output "route-table-default"{
+  value = aws_vpc.vpc.default_route_table_id
 }
 output "route-tables" {
   value = concat([aws_vpc.vpc.default_route_table_id], aws_route_table.private.*.id, aws_route_table.public.*.id)
