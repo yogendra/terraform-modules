7c4ba3c9a1f80024861bdd3892825f137d0b192f
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2023-05-05 22:17:13 +0800

    Update to allow only traffic from VPC

--- yugabyte/infra/aws/vpc/endpoints.tf
+++ yugabyte/infra/aws/vpc/endpoints.tf
@@ -4,21 +4,36 @@ resource "aws_vpc_endpoint" "s3" {
   vpc_id            = aws_vpc.vpc.id
   service_name      = "com.amazonaws.${local.region}.s3"
   vpc_endpoint_type = "Gateway"
-  route_table_ids   = [aws_route_table.public.id,aws_route_table.private.id, aws_vpc.vpc.default_route_table_id]
+  route_table_ids   = [aws_route_table.private.id,aws_route_table.private.id, aws_vpc.vpc.default_route_table_id]
   tags = {
     Name = "${var.project_config.prefix}-s3"
   }
 }
 
 
+resource "aws_security_group" "allow-vpc-internal" {
+  name        = "${var.project_config.prefix}-vpc-internal"
+  description = "Allow Internal VPC traffic only"
+  vpc_id      = aws_vpc.vpc.id
+  ingress {
+    description      = "Allow HTTPS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = var.config.cidrs
+  }
+  tags = {
+    Name = "${var.project_config.prefix}-vpc-internal"
+  }
+}
+
 
 resource "aws_vpc_endpoint" "ssm" {
-  # count               = local.create_endpoints?1:0
   vpc_id              = aws_vpc.vpc.id
   service_name        = "com.amazonaws.${local.region}.ssm"
   vpc_endpoint_type   = "Interface"
-  security_group_ids  = [aws_security_group.allow-internal.id]
-  subnet_ids          = aws_subnet.public[*].id
+  security_group_ids  = [aws_security_group.allow-vpc-internal.id]
+  subnet_ids          = aws_subnet.private[*].id
   private_dns_enabled = true
   tags = {
     Name = "${var.project_config.prefix}-ssm"
@@ -26,12 +41,11 @@ resource "aws_vpc_endpoint" "ssm" {
 }
 
 resource "aws_vpc_endpoint" "ec2messages" {
-  # count               = local.create_endpoints?1:0
   vpc_id              = aws_vpc.vpc.id
   service_name        = "com.amazonaws.${local.region}.ec2messages"
   vpc_endpoint_type   = "Interface"
-  security_group_ids  = [aws_security_group.allow-internal.id]
-  subnet_ids          = aws_subnet.public[*].id
+  security_group_ids  = [aws_security_group.allow-vpc-internal.id]
+  subnet_ids          = aws_subnet.private[*].id
   private_dns_enabled = true
   tags = {
 
@@ -39,12 +53,11 @@ resource "aws_vpc_endpoint" "ec2messages" {
   }
 }
 resource "aws_vpc_endpoint" "ssmmessages" {
-  # count               = local.create_endpoints?1:0
   vpc_id              = aws_vpc.vpc.id
   service_name        = "com.amazonaws.${local.region}.ssmmessages"
   vpc_endpoint_type   = "Interface"
-  security_group_ids  = [aws_security_group.allow-internal.id]
-  subnet_ids          = aws_subnet.public[*].id
+  security_group_ids  = [aws_security_group.allow-vpc-internal.id]
+  subnet_ids          = aws_subnet.private[*].id
   private_dns_enabled = true
   tags = {
     Name = "${var.project_config.prefix}-ssmmessages"
@@ -53,12 +66,11 @@ resource "aws_vpc_endpoint" "ssmmessages" {
 
 
 resource "aws_vpc_endpoint" "ec2" {
-  # count             = local.create_endpoints?1:0
   vpc_id            = aws_vpc.vpc.id
   service_name      = "com.amazonaws.${local.region}.ec2"
   vpc_endpoint_type = "Interface"
-  security_group_ids  = [aws_security_group.allow-internal.id]
-  subnet_ids          = aws_subnet.public[*].id
+  security_group_ids  = [aws_security_group.allow-vpc-internal.id]
+  subnet_ids          = aws_subnet.private[*].id
   private_dns_enabled = true
   tags = {
     Name = "${var.project_config.prefix}-ec2"
@@ -67,12 +79,11 @@ resource "aws_vpc_endpoint" "ec2" {
 
 
 resource "aws_vpc_endpoint" "kms" {
-  # count             = local.create_endpoints?1:0
   vpc_id            = aws_vpc.vpc.id
   service_name      = "com.amazonaws.${local.region}.kms"
   vpc_endpoint_type = "Interface"
-  security_group_ids  = [aws_security_group.allow-internal.id]
-  subnet_ids          = aws_subnet.public[*].id
+  security_group_ids  = [aws_security_group.allow-vpc-internal.id]
+  subnet_ids          = aws_subnet.private[*].id
   private_dns_enabled = true
   tags = {
     Name = "${var.project_config.prefix}-kms"
@@ -81,12 +92,11 @@ resource "aws_vpc_endpoint" "kms" {
 
 
 resource "aws_vpc_endpoint" "cloudwatch" {
-  # count             = local.create_endpoints?1:0
   vpc_id            = aws_vpc.vpc.id
   service_name      = "com.amazonaws.${local.region}.logs"
   vpc_endpoint_type = "Interface"
-  security_group_ids  = [aws_security_group.allow-internal.id]
-  subnet_ids          = aws_subnet.public[*].id
+  security_group_ids  = [aws_security_group.allow-vpc-internal.id]
+  subnet_ids          = aws_subnet.private[*].id
   private_dns_enabled = true
   tags = {
     Name = "${var.project_config.prefix}-cloudwatch"
