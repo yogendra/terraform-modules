a647df6f391dc7ada71fbfc0fa465b8658918f52
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2023-05-07 20:04:56 +0800

    Change nat gw to nat isntance

--- yugabyte/infra/aws/vpc/gw.tf
+++ yugabyte/infra/aws/vpc/gw.tf
@@ -3,6 +3,8 @@ resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.vpc.id
   tags = {
     "Name" = "${var.project_config.prefix}-igw"
+    yb_aws_service = "vpc"
+    yb_resource_type = "igw"
   }
 }
 
@@ -10,40 +12,34 @@ resource "aws_eip" "nat"{
   count = local.create_nat_gw ? 1 : 0
   tags = {
     Name = "${var.project_config.prefix}-nat"
+    yb_aws_service = "ec2"
+    yb_resource_type = "eip"
   }
 }
 
-
-resource "aws_nat_gateway" "nat" {
+module "nat" {
   count = local.create_nat_gw ? 1 : 0
-  allocation_id = aws_eip.nat[0].id
-  subnet_id     = aws_subnet.public[0].id
+  source = "int128/nat-instance/aws"
 
+  name                        = "${var.project_config.prefix}-nat"
+  vpc_id                      = aws_vpc.vpc.id
+  key_name                    = aws_key_pair.keypair[0].key_name
+  instance_types              = lookup(local.nat-instance-types-override, local.region, local.nat-instance-types-override.default )
+  public_subnet               = aws_subnet.public[0].id
+  private_subnets_cidr_blocks = local.private_subnets[*].cidr
+  private_route_table_ids     = [aws_route_table.private.id, aws_default_route_table.default.id]
+  use_spot_instance           = true
   tags = {
     Name = "${var.project_config.prefix}-nat"
+    yb_aws_service = "vpc"
+    yb_resource_type = "nat"
   }
-
-  depends_on = [aws_internet_gateway.igw]
 }
+
 
 resource "aws_route" "public-egress" {
   count = local.create_igw ? 1 : 0
   route_table_id = aws_route_table.public.id
   destination_cidr_block    = "0.0.0.0/0"
   gateway_id = aws_internet_gateway.igw[0].id
-}
-
-resource "aws_route" "private-egress" {
-  count = local.create_nat_gw ? 1 : 0
-  route_table_id = aws_route_table.private.id
-  destination_cidr_block    = "0.0.0.0/0"
-  nat_gateway_id = aws_nat_gateway.nat[0].id
-}
-
-
-resource "aws_route" "default-egress" {
-  count = local.create_nat_gw ? 1 : 0
-  route_table_id = aws_default_route_table.default.id
-  destination_cidr_block    = "0.0.0.0/0"
-  nat_gateway_id = aws_nat_gateway.nat[0].id
 }
