99188cc0275436f0131d16d829196bf3198d19de
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2023-05-07 20:05:22 +0800

    Add resource tags

--- yugabyte/infra/aws/vpc/keypair.tf
+++ yugabyte/infra/aws/vpc/keypair.tf
@@ -2,5 +2,10 @@ resource "aws_key_pair" "keypair" {
   count = length(var.project_config.ssh-public-keys)
   key_name = "${var.project_config.prefix}-${count.index}"
   public_key = var.project_config.ssh-public-keys[count.index]
+  tags = {
+    Name = "${var.project_config.prefix}-${count.index}"
+    yb_aws_service = "ec2"
+    yb_resource_type = "keypair"
+  }
 }
 
