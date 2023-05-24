19ff187cc426f9180d6aeed29b6e24ac9c6abcf8
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2023-05-24 23:22:49 +0800

    updated output include arn and name for instance role

--- yugabyte/infra/aws/account/output.tf
+++ yugabyte/infra/aws/account/output.tf
@@ -1,7 +1,15 @@
 output "yba-instance-profile" {
+  value = aws_iam_instance_profile.yba.name
+}
+output "yba-instance-profile-arn" {
   value = aws_iam_instance_profile.yba.arn
 }
 
+
 output "ssm-instance-profile" {
+  value = aws_iam_instance_profile.ssm.name
+}
+
+output "ssm-instance-profile-arn" {
   value = aws_iam_instance_profile.ssm.arn
 }
