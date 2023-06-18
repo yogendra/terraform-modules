cda044c9446ebdbee42e7444e04a33e80b66bf48
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2023-06-18 16:36:56 +0800

    update demobox definition

--- yugabyte/infra/aws/demobox/output.tf
+++ yugabyte/infra/aws/demobox/output.tf
@@ -1,15 +1,12 @@
-
-output "vm" {
-  value =  aws_instance.demobox
-}
-
-output "vm-private-ip" {
-  value =  aws_instance.demobox.private_ip
-}
-
-output "vm-public-ip" {
-  value =  aws_instance.demobox.public_ip
-}
 output "vm-instance-id" {
-  value =  aws_instance.demobox.id
+  value =  aws_instance.vm.id
+}
+output "vm-private-ip" {
+  value =  aws_instance.vm.private_ip
+}
+output "vm-public-ip" {
+  value =  aws_instance.vm.public_ip
+}
+output "vm-fqdn" {
+  value =  length(var.aws-hosted-zone-name) > 0 ? aws_route53_record.vm-dns[0].fqdn : ""
 }
--- yugabyte/infra/aws/demobox/variables.tf
+++ yugabyte/infra/aws/demobox/variables.tf
@@ -2,14 +2,6 @@ variable "prefix" {
   type = string
   description = "Prefix for resources"
 }
-variable "name" {
-  type = string
-  description = "app name"
-}
-variable "asset-bucket" {
-  type = string
-  description = "Asset bucket name"
-}
 variable "asset-bucket-location" {
   type = string
   description = "Asset location inside the bucket"
@@ -20,38 +12,53 @@ variable "password" {
   description = "Password for resources"
   default = "Password#123"
 }
-
-variable "machine-type" {
+variable "hostname" {
   type = string
-  description = "Machine type"
-  default = "t3.2xlarge"
-}
-
-variable "security-group-ids" {
-  type = list(string)
-  description = "Security Groups"
-  default = ["default"]
+  description = "Hostname for app VM. If not given, it is generated automatically based on hosted zone, prefix and app name"
+  default = ""
 }
 
-variable "subnet-id" {
-  type = string
-  description = "Subnet"
-}
-variable "keypair-name" {
-  type = string
-  description = "Key pair name"
-}
-variable "instance-profile" {
-  type = string
-  description = "Instance Profile"
-}
 variable "tags"{
   type = map(string)
   description = "Tags to put on the machines"
   default = {}
 }
-variable "ami" {
+variable "aws-asset-bucket" {
   type = string
-  description = "Machine image for vm"
+  description = "Asset bucket name"
+}
+variable "aws-machine-type" {
+  type = string
+  description = "AWS Machine type. Optional, t3.2xlarge is used by default"
+  default = "t3.2xlarge"
+}
+variable "aws-security-group-ids" {
+  type = list(string)
+  description = "AWs Security Groups"
+  default = ["default"]
+}
+variable "aws-hosted-zone-name" {
+  type = string
+  description = "AWS Hosted Zone Name. Optional. If specified, an A record will be created in the hosted zone"
   default = ""
 }
+
+variable "aws-subnet-id" {
+  type = string
+  description = "AWS Subnet"
+}
+variable "aws-keypair-name" {
+  type = string
+  description = "AWS Key pair name"
+}
+variable "aws-instance-profile" {
+  type = string
+  description = "AWS Instance Profile"
+}
+variable "aws-ami" {
+  type = string
+  description = "AWS Machine image for vm. Optional, uses Ubuntu jammy latest image, if not specified"
+  default = ""
+}
+
+
