d45d94e3989be4c46ef615e99a01a198b67c2866
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2023-07-23 15:12:53 +0800

    Add direnv support and ssh command output

--- yugabyte/infra/aws/jumpbox/output.tf
+++ yugabyte/infra/aws/jumpbox/output.tf
@@ -10,3 +10,6 @@ output "vm-public-ip" {
 output "vm-fqdn" {
   value =  length(var.aws-hosted-zone-name) > 0 ? aws_route53_record.vm-dns[0].fqdn : ""
 }
+output "ssh-command-template" {
+  value = var.assign-public-ip ?  "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $PRIVATE_KEY ubuntu@${aws_instance.vm.public_ip}" : "ssh ubuntu@ssm.${aws_instance.vm.id}.$AWS_PROFILE.${data.aws_region.current.name}"
+}
