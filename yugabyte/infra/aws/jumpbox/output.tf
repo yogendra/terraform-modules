f95b029708c758793cb837b141ccf9e8371eec98
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2023-07-24 01:21:30 +0800

    add ssh-config-template to output

--- yugabyte/infra/aws/jumpbox/output.tf
+++ yugabyte/infra/aws/jumpbox/output.tf
@@ -13,3 +13,16 @@ output "vm-fqdn" {
 output "ssh-command-template" {
   value = var.assign-public-ip ?  "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $PRIVATE_KEY ubuntu@${aws_instance.vm.public_ip}" : "ssh ubuntu@ssm.${aws_instance.vm.id}.$AWS_PROFILE.${data.aws_region.current.name}"
 }
+
+output "ssh-config-template" {
+  value =<<EOF
+Host ${var.prefix}-jb
+  Hostname ssm.${aws_instance.vm.id}.$AWS_PROFILE.${data.aws_region.current.name}
+  IdentityFile  $PRIVATE_KEY
+  User ubuntu
+  UserKnownHostsFile /dev/null
+  StrictHostKeyChecking no
+  ProxyCommand bash -c "aws ec2-instance-connect send-ssh-public-key --profile $(echo %h|cut -d'.' -f3)  --region $(echo %h|cut -d'.' -f4) --instance-os-user %r --instance-id  $(echo %h|cut -d'.' -f2) --ssh-public-key file://$PRIVATE_KEY.pub &> /dev/null  ;aws ssm start-session --target $(echo %h|cut -d'.' -f2) --profile $(echo %h|cut -d'.' -f3) --region $(echo %h|cut -d'.' -f4) --document-name AWS-StartSSHSession --parameters 'portNumber=%p'"
+
+EOF
+}
