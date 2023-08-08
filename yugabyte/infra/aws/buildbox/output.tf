5e1918238d023b2828a120b3dcaf702dd8502825
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2023-08-08 19:50:35 +0800

    update direct-ssh instruction to use yugabtye user

--- yugabyte/infra/aws/buildbox/output.tf
+++ yugabyte/infra/aws/buildbox/output.tf
@@ -10,3 +10,13 @@ output "vm-public-ip" {
 output "vm-fqdn" {
   value =  length(var.aws-hosted-zone-name) > 0 ?  aws_route53_record.vm-dns[0].fqdn : ""
 }
+
+output "vm-ssh-command-template" {
+  value = <<EOF
+  SSM        : ssh ubuntu@ssm.${aws_instance.vm.id}.$AWS_PROFILE.${data.aws_region.current.name}
+%{ if aws_eip.vm-ip.public_ip  != "" ~}
+  Direct SSH : ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i $PRIVATE_KEY yugabyte@${aws_eip.vm-ip.public_ip}
+               ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i $PRIVATE_KEY yugabyte@${ aws_route53_record.vm-dns[0].fqdn}
+%{ endif ~}
+EOF
+}
