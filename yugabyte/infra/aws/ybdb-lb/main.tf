4ec7627f56064d100ae999b6289473ac39d2b45a
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2023-08-29 00:27:16 +0800

    add lb module

---
+++ yugabyte/infra/aws/ybdb-lb/main.tf
@@ -0,0 +1,44 @@
+
+
+locals {
+  nginx-config = templatefile("./templates/nginx.conf", {
+    listener-port     = var.listener-port
+    forward-addresses = var.forward-addresses
+  })
+
+}
+module "lb" {
+
+  source                     = "../generic-box"
+  arch                       = var.arch
+  hostname                   = var.hosstname
+  aws-ami                    = var.aws-ami
+  assign-public-ip           = var.assign-public-ip
+  aws-instance-profile       = var.aws-instance-profile
+  aws-keypair-name           = var.keypair-name
+  aws-machine-type           = var.aws-machine-type
+  aws-security-group-ids     = var.aws-security-group-ids
+  aws-subnet-id              = var.aws-subnet-id
+  aws-public-hosted-zone-id  = var.aws-public-hosted-zone-id
+  aws-private-hosted-zone-id = var.aws-private-hosted-zone-id
+  disk-count                 = 0
+  files = [
+    {
+      path    = "/etc/nginx/nginx.conf"
+      content = local.nginx-config
+      defer   = true
+    }
+  ]
+  name = "dssmdcmlb"
+  packages = [
+    "nginx"
+  ]
+  startup-commands = [
+    "sudo systemctl enable nginx",
+    "sudo systemctl start nginx"
+  ]
+  tags = merge(var.tags, {
+    yb_component = "lb"
+  })
+  prefix = var.prefix
+}
---
+++ yugabyte/infra/aws/ybdb-lb/variables.tf
@@ -0,0 +1,80 @@
+variable "listener-ports" {
+  type = number
+  description = "Listener port"
+  default = [5433]
+}
+variable "forward-addresses" {
+  type = list(number)
+  description = "List of forward for each listner port"
+  default = null
+}
+
+variable "prefix" {
+  type        = string
+  description = "Prefix for resources"
+}
+variable "name" {
+  type        = string
+  description = "Name"
+}
+variable "arch" {
+  type        = string
+  description = "Processor"
+  default     = "x86_64"
+}
+variable "hostname" {
+  type        = string
+  description = "Hostname for app VM. If not given, it is generated automatically based on hosted zone, prefix and app name"
+  default     = ""
+}
+variable "tags" {
+  type        = map(string)
+  description = "Tags to put on the machines"
+  default     = {}
+}
+variable "aws-machine-type" {
+  type        = string
+  description = "AWS Machine type"
+  default     = "t3.2xlarge"
+}
+variable "aws-security-group-ids" {
+  type        = list(string)
+  description = "AWS Security Groups"
+}
+variable "assign-public-ip" {
+  type        = bool
+  description = "Whether to assign public IP or not"
+  default     = false
+}
+
+variable "aws-public-hosted-zone-id" {
+  type        = string
+  description = "AWS Public Hosted Zone ID"
+  default     = null
+}
+
+variable "aws-private-hosted-zone-id" {
+  type        = string
+  description = "AWS Privated Hosted Zone ID"
+  default     = null
+}
+
+variable "aws-subnet-id" {
+  type        = string
+  description = "Subnet"
+}
+variable "aws-keypair-name" {
+  type        = string
+  description = "Key pair name"
+  default = null
+}
+variable "aws-instance-profile" {
+  type        = string
+  description = "AWS Instance Profile"
+  default = null
+}
+variable "aws-ami" {
+  type        = string
+  description = "AWS Machine image for vm"
+  default     = ""
+}
