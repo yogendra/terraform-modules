4e3459625d74f1e72606b0438c7f0649944d6eb7
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2023-09-08 03:27:26 +0800

    fixed formatting

--- yugabyte/infra/aws/generic-box/output.tf
+++ yugabyte/infra/aws/generic-box/output.tf
@@ -17,18 +17,18 @@ output "vm-private-fqdn" {
 
 output "vm-ssh-command-template" {
   value = <<EOF
-  SSH over SSM  : (Set the ssh config for Host ssm.i*)
-                  ssh -i $PRIVATE_KEY yugabyte@ssm.${aws_instance.vm.id}.$AWS_PROFILE.${data.aws_region.current.name}
+SSH over SSM  : (Set the ssh config for Host ssm.i*)
+                ssh -i $PRIVATE_KEY yugabyte@ssm.${aws_instance.vm.id}.$AWS_PROFILE.${data.aws_region.current.name}
 
-  Direct SSH    : ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i $PRIVATE_KEY yugabyte@${aws_network_interface.eni.private_ip}
+Direct SSH    : ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i $PRIVATE_KEY yugabyte@${aws_network_interface.eni.private_ip}
 %{if var.aws-private-hosted-zone-id != null ~}
-                  ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i $PRIVATE_KEY yugabyte@${aws_route53_record.vm-private-dns[0].fqdn}
+                ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i $PRIVATE_KEY yugabyte@${aws_route53_record.vm-private-dns[0].fqdn}
 %{endif~}
 %{if var.assign-public-ip ~}
-                  ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i $PRIVATE_KEY yugabyte@${aws_eip.vm-public-ip[0].public_ip}
+                ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i $PRIVATE_KEY yugabyte@${aws_eip.vm-public-ip[0].public_ip}
 %{endif~}
 %{if var.aws-public-hosted-zone-id != null ~}
-                  ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i $PRIVATE_KEY yugabyte@${aws_route53_record.vm-public-dns[0].fqdn}
+                ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i $PRIVATE_KEY yugabyte@${aws_route53_record.vm-public-dns[0].fqdn}
 %{endif~}
 EOF
 }
