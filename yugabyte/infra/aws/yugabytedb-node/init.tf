da241efd0df0ffe6e6e229358664d3a90aa2cb15
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2023-09-08 03:29:31 +0800

    Add support for precreated ami

--- yugabyte/infra/aws/yugabytedb-node/init.tf
+++ yugabyte/infra/aws/yugabytedb-node/init.tf
@@ -13,12 +13,15 @@ data "aws_subnet" "subnet" {
 
 
 locals {
-  yb-version  = split("-", var.config.yb-release)[0]
+  use-prebuilt = var.config.aws-ami == ""
+  aws-ami = local.use-prebuilt? local.pre-built-amis[var.config.yb-version][var.config.arch][data.aws_region.current.name]:""
+
   join-master = var.config.join-master == null ? "" : var.config.join-master
 
   ybd-join-param       = var.config.join-master == null ? {} : { join = var.config.join-master }
   ydb-cloud-location   = { cloud_location = "aws.${data.aws_region.current.name}.${data.aws_subnet.subnet.availability_zone}" }
   yugabyted-parameters = merge(local.ybd-join-param, local.ydb-cloud-location, var.config.yugabyted-parameters)
+
   start-yugabyted = templatefile("${path.module}/templates/start_yugabyted.sh", {
     parameters     = local.yugabyted-parameters
     yugabyted-conf = var.config.yugabyted-config
@@ -28,38 +31,47 @@ locals {
     replication-factor = var.config.replication-factor
     fault-tolerance = var.config.fault-tolerance
   })
-  scripts = [
-    {
-      path        = "/home/yugabyte/bin/install_software.sh"
-      binary      = false
-      owner       = "yugabyte:yugabyte"
-      permissions = "0700"
-      content     = file("${path.module}/scripts/install_software.sh")
-      defer       = true
-      append      = false
-    },
-    {
-      path        = "/home/yugabyte/bin/start_yugbayted.sh"
-      binary      = false
-      owner       = "yugabyte:yugabyte"
-      permissions = "0700"
-      content     = local.start-yugabyted
-      defer       = true
-      append      = false
-    },
-  ]
+
+  install-script =  {
+    path        = "/home/yugabyte/bin/install_software.sh"
+    binary      = false
+    owner       = "yugabyte:yugabyte"
+    permissions = "0700"
+    content     = file("${path.module}/scripts/install_software.sh")
+    defer       = true
+    append      = false
+  }
+
+  start-script = {
+    path        = "/home/yugabyte/bin/start_yugbayted.sh"
+    binary      = false
+    owner       = "yugabyte:yugabyte"
+    permissions = "0700"
+    content     = local.start-yugabyted
+    defer       = true
+    append      = false
+  }
+
   files           = concat(local.scripts, var.config.files)
   yugabyted-start = "sudo -iu yugabyte /home/yugabyte/bin/start_yugbayted.sh"
-  startup-commands = [
-    "sudo ln -s /usr/bin/python3 /usr/bin/python",
-    "sudo -iu yugabyte /home/yugabyte/bin/install_software.sh ${local.yb-version}",
+
+  prebuild-startup-commands = [
+    "chown -R yugabyte:yugabyte /home/yugabyte/data",
+    "chmod 700 /home/yugabyte/data",
     local.yugabyted-start
   ]
+  custom-startup-comamnds = concat([
+    "sudo ln -s /usr/bin/python3 /usr/bin/python",
+    "sudo -iu yugabyte /home/yugabyte/bin/install_software.sh ${var.config.yb-version}",
+  ], local.prebuild-startup-commands )
+
+  startup-commands = local.use-prebuilt? local.prebuild-startup-commands : local.custom-startup-comamnds
   boot-commands = [
     local.yugabyted-start
   ]
 
-  packages = [
+  scripts = local.use-prebuilt ? [ local.start-script] : [local.install-script, local.start-script]
+  packages = local.use-prebuilt ? [] :  [
     "wget",
     "python3"
   ]
